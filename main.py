282c4cd4adbb865b30cba7b3c013efceea8ae30c
BobbyBoy101 <132041117+BobbyBoy101@users.noreply.github.com>
2023-07-24 03:06:20 -0600

    No moon rise/set fix
    Fixes the problem when there is no moon rise/set in a day, but creates new problem for last 2 days of the month.

--- main.py
+++ main.py
@@ -14,12 +14,15 @@ ROW_OFFSET = 1
 # Make an observer
 obs = ephem.Observer()
 
-# lat = input("Enter your latitude: ")
-# lon = input("Enter your longitude: ")
+"""
+Disabled lat/lon and date input prompt so you don't have to type them in every time while troubleshooting
+lat = input("Enter your latitude: ")
+lon = input("Enter your longitude: ")
+start_date = parser.parse(input("Enter the start date (YYYY/MM/DD): "))
+end_date = parser.parse(input("Enter the end date (YYYY/MM/DD): "))
+"""
 lat = '40.7720'
 lon = '-112.1012'
-# start_date = parser.parse(input("Enter the start date (YYYY/MM/DD): "))
-# end_date = parser.parse(input("Enter the end date (YYYY/MM/DD): "))
 start_date = parser.parse('2023/04/01')
 end_date = parser.parse('2023/05/01')
 
@@ -35,6 +38,7 @@ end_twilight_times = []
 begin_twilight_times = []
 moonrise_times = []
 
+# Note: The Ephem library ALWAYS uses Universal Time (UTC), never the local time zone, which complicates things
 while current_utc < end_utc:
 
     obs.date = current_utc
@@ -44,8 +48,8 @@ while current_utc < end_utc:
     moonset_time = obs.next_setting(ephem.Moon(), start=current_utc)
     moonrise_time = obs.previous_rising(ephem.Moon(), start=current_utc)
 
-    # Calculate the astronomical twilight times
-    obs.horizon = '-18'  # astronomical twilight
+    # Calculate the astronomical twilight times (when sun is -18 below horizon)
+    obs.horizon = '-18'
     begin_twilight = obs.previous_rising(ephem.Sun(), start=current_utc, use_center=True)
     end_twilight = obs.next_setting(ephem.Sun(), start=current_utc, use_center=True)
 
@@ -59,7 +63,7 @@ while current_utc < end_utc:
     obs.date = moonset_time
     moonset_time_local = ephem.localtime(obs.date).strftime('%Y/%m/%d %I:%M %p')
 
-    # add the local time zone moonset/rise and astronomical twilight begin/end times to the list
+    # Add the local time zone moonset/rise and astronomical twilight begin/end times to the list
     moonset_times.append(moonset_time.datetime())
     end_twilight_times.append(end_twilight.datetime())
     begin_twilight_times.append(begin_twilight.datetime())
@@ -80,7 +84,7 @@ def main():
     workbook = xlsxwriter.Workbook("darkSkyTimes.xlsx")
     worksheet = workbook.add_worksheet('DarkSkyTimes')
 
-    # format cells
+    # Format cells
     cell_header_format = workbook.add_format()
     cell_header_format.set_bold()
     cell_header_format.set_text_wrap()
@@ -89,13 +93,8 @@ def main():
     time_format = workbook.add_format()
     time_format.set_text_wrap()
     time_format.set_align('center_across')
-    # time_format.set_num_format('h:mm;@')
-    # time_format.set_num_format('[$-en-US]h:mm AM/PM;@')
-    
-    # green_time_format = workbook.add_format({'num_format': 'h:mm AM/PM'})
-    # green_time_format.set_font_color('green')
 
-    # create headers using cell location, then what you want to name it
+    # Create headers using cell location
     worksheet.write('A2', 'Day', cell_header_format)
     worksheet.write('B2', 'Moon Set', cell_header_format)
     worksheet.write('C2', 'Astronomical Twilight END', cell_header_format)
@@ -112,6 +111,18 @@ def main():
         moon_set_str = moon_set.strftime('%Y/%m/%d %H:%M')
         local_moon_set_day = moon_set.astimezone(pytz.timezone('US/Mountain')).day
 
+        """
+        On days when the moon does not rise or does not set in that day, then that cell should be blank.
+        However, the rise/set time for the next day was being populated in that cell that should be blank.
+        This solves that problem, but creates a new problem where the last day or two of the month is
+        skipping a day or being displayed on the wrong day.
+
+        Current problem:
+        Second to last day of month: incorrect moon rise time, it displays the moon rise time for the next day.
+        Last day of month: incorrect moon set time, it displays the time for the next day (which is the first day
+        of the next month). Also, no data is being filled in for the last day of the month for the moon rise time,
+        because it is being inputted on the second to last day of the month, so that cell is blank when it shouldn't be.
+        """
         moon_rise_tz = moonrise_times[row].replace(tzinfo=pytz.timezone('UTC'))
         moon_rise = moon_rise_tz.astimezone(pytz.timezone('US/Mountain'))
         moon_rise_str = moon_rise.strftime('%Y/%m/%d %H:%M')
@@ -142,7 +153,7 @@ def main():
         rowIndex += 1
 
 
-    # Set column size. Numbering starts at 0
+    # Set column size
     worksheet.set_column('A:A', 5)
     worksheet.set_column('B:B', 20)
     worksheet.set_column('C:C', 23)
@@ -158,11 +169,11 @@ def main():
     # Get the active sheet
     sheet = workbook.active
 
-    # Delete the row of times that is generated before the specified date-range
+    # Delete the row of times that is generated before the specified date-range since Ephem uses "previous_rising"
     sheet.delete_rows(3)
 
     # Save the modified workbook
-    workbook.save('DltRow_darkSkyTimes.xlsx')
+    workbook.save('darkSkyTimes.xlsx')
 
 
 if __name__ == "__main__":
